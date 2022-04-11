1f94e585d83cb357a7eeb2cfde84b408f2e6e0c0
Maslov, Oleh <oleh_maslov_tp@bmc.com>
2022-04-11 13:37:40 +0300

    Parsing Query Strings

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer(function (req, res) {
-  // Get the url and parse it
+  // Get the url and parse it.
+  // With second argument equal 'false', query string will be parsed as string: testParam=testValue, testParam=testValue&anotherParam=anotherValue.
+  // With secong argument equal 'true', query string will be parsed as key-value object: {testParam: 'testValue'}, {testParam: 'testValue', anotherParam: 'anotherValue'}
   const parsedUrl = url.parse(req.url, true);
 
   // Get the path
   const path = parsedUrl.pathname;
 	const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
 
   // Get HTTP method
   const method = req.method.toLowerCase();
@@ -17,6 +22,7 @@ const server = http.createServer(function (req, res) {
 
   // Log the request path
   console.log(`Request received on path: ${trimmedPath}, with method: ${method}`);
+  console.log('and query params:', queryStringObject);
 });
 
 server.listen(3000, () => {
