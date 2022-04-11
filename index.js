694efc82c4cf82eed698d1b8b2d98161e7a3ad1b
Maslov, Oleh <oleh_maslov_tp@bmc.com>
2022-04-11 13:34:40 +0300

    Parsing Request Paths

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer(function (req, res) {
-  res.end("Hello World!");
+  // Get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+	console.log('path', path);
+	const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+	console.log('trimmedPath', trimmedPath);
+
+  // Send the response
+  res.end('Hello World!\n');
+
+  // Log the request path
 });
 
 server.listen(3000, () => {
